3a0a7d677cd824d022830b8a40f23d260f6eb942
takeshix <takeshix@adversec.com>
2016-04-23 23:41:04 +0200

    Added file for ObjectServer implementation

---
+++ libknx/objectserver.py
@@ -0,0 +1,7 @@
+"""Implementation of the ObjectServer service communication.
+
+Infos about the ObjectServer protocol:
+    http://www.weinzierl.de/download/products/770/KNX_BAOS_Protocol.pdf
+
+ObjectServer can either be included in KNXnet/IP packets (with an appropriate
+connection type, or via TCP on port 12004."""
