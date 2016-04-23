cbccd5f508c3dba1219497168d84bff939ba9a20
takeshix <takeshix@adversec.com>
2016-04-23 23:42:21 +0200

    Added busmonitor skeleton

--- libknx/bus.py
+++ libknx/bus.py
@@ -10,8 +10,11 @@ from .messages import *
 LOGGER = logging.getLogger(__name__)
 
 
-class KnxBusConnection(asyncio.DatagramProtocol):
+class KnxTunnelConnection(asyncio.DatagramProtocol):
     """Communicate with bus devices via a KNX gateway using TunnellingRequests."""
+
+    # TODO: a tunnelling connection is always used when the destination
+    # is a physical KNX address
 
     tunnel_established = False
     communication_channel = None
@@ -23,7 +26,6 @@ class KnxBusConnection(asyncio.DatagramProtocol):
         self.loop = loop or asyncio.get_event_loop()
         self.transport = None
 
-
     def connection_made(self, transport):
         self.transport = transport
         self.peername = self.transport.get_extra_info('peername')
@@ -34,8 +36,6 @@ class KnxBusConnection(asyncio.DatagramProtocol):
         connect_request = KnxConnectRequest(sockname=self.sockname)
         connect_request.pack_knx_message()
         self.transport.sendto(connect_request.get_message())
-        LOGGER.debug('KnxConnectionRequest sent')
-
 
     def datagram_received(self, data, addr):
         LOGGER.debug('data: {}'.format(data))
@@ -53,10 +53,8 @@ class KnxBusConnection(asyncio.DatagramProtocol):
             self.transport.close()
             return
 
-        LOGGER.info('message_type: {}'.format(message_type))
-
         if message_type == 0x0206: # CONNECT_RESPONSE
-            LOGGER.info('Parsing KnxConnectResponse')
+            LOGGER.debug('Parsing KnxConnectResponse')
             response = KnxConnectResponse(data)
 
             if not response.ERROR:
@@ -73,19 +71,21 @@ class KnxBusConnection(asyncio.DatagramProtocol):
                 self.future.set_result(None)
 
         elif message_type == 0x0420: # TUNNELLING_REQUEST
-            # TODO: why does the gateway send back TUNNELLING_REQUESTS?
-            LOGGER.info('Parsing KnxTunnelingRequest')
+            # KNXnet/IP gateway sends back confirmation messages in TUNNELLING_REQUESTS
+            LOGGER.debug('Parsing KnxTunnelingRequest')
             response = KnxTunnellingRequest(data)
 
-            tunnelling_ack = KnxTunnellingAck(
-                communication_channel=response.body.get('communication_channel_id'),
-                sequence_count=response.body.get('sequence_counter'))
-            tunnelling_ack.pack_knx_message()
-            self.transport.sendto(tunnelling_ack.get_message())
+            if CEMI_PRIMITIVES[response.body.get('cemi').get('message_code')] == 'L_Data.con':
+                # Only if it's a confirmation message we will respond
+                tunnelling_ack = KnxTunnellingAck(
+                    communication_channel=response.body.get('communication_channel_id'),
+                    sequence_count=response.body.get('sequence_counter'))
+                tunnelling_ack.pack_knx_message()
+                self.transport.sendto(tunnelling_ack.get_message())
 
 
         elif message_type == 0x0421: # TUNNELLING_ACK
-            LOGGER.info('Parsing KnxTunnelingAck')
+            LOGGER.debug('Parsing KnxTunnelingAck')
             response = KnxTunnellingAck(data)
 
             # TODO: probably not even needed
@@ -93,11 +93,11 @@ class KnxBusConnection(asyncio.DatagramProtocol):
 
 
         elif message_type == 0x0209: # DISCONNECT_REQUEST
-            LOGGER.info('Parsing KnxDisconnectRequest')
+            LOGGER.debug('Parsing KnxDisconnectRequest')
             response = KnxDisconnectResponse(data)
 
         elif message_type == 0x020a: # DISCONNECT_RESPONSE
-            LOGGER.info('Parsing KnxDisconnectResponse')
+            LOGGER.debug('Parsing KnxDisconnectResponse')
             response = KnxDisconnectResponse(data)
             self.transport.close()
         else:
@@ -112,3 +112,49 @@ class KnxBusConnection(asyncio.DatagramProtocol):
             communication_channel=self.communication_channel)
         disconnect_request.pack_knx_message()
         self.transport.sendto(disconnect_request.get_message())
+
+
+class KnxRoutingConnection(asyncio.DatagramProtocol):
+
+    # TODO: routing is used to send KNX messages to multiple devices
+    # without any connection setup (in contrast to tunnelling)
+
+    # TODO: uses UDP multicast (224.0.23.12) packets to port 3671
+
+    # TODO: no confirmation of successful transmission
+
+    # TODO: will send message to group address if supplied group address
+    # is known by devices?
+
+    def __init__(self, future, loop=None):
+        self.future = future
+        self.loop = loop or asyncio.get_event_loop()
+        self.transport = None
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        pass
+
+    def _send(self, message):
+        self.transport.get_extra_info('socket').sendto(message.get_message(), ('224.0.23.12', 3671))
+
+
+class KnxBusMonitor(asyncio.DatagramProtocol):
+
+    # TODO: monitors messages from the bus
+
+    def __init__(self, future, loop=None):
+        self.future = future
+        self.loop = loop or asyncio.get_event_loop()
+        self.transport = None
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        pass
+
+    def _send(self, message):
+        self.transport.get_extra_info('socket').sendto(message.get_message(), ('224.0.23.12', 3671))
