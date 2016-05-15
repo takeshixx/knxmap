7404fc732fc2d013aa5b51d86a932de5d0ab88ac
takeshix <takeshix@adversec.com>
2016-05-15 19:05:09 +0200

    Proper --bus-monitor and --group-monitor implementations

--- libknx/bus.py
+++ libknx/bus.py
@@ -33,24 +33,11 @@ class KnxTunnelConnection(asyncio.DatagramProtocol):
         LOGGER.debug('Connection established')
 
         connect_request = KnxConnectRequest(sockname=self.sockname)
-        connect_request.pack_knx_message()
         self.transport.sendto(connect_request.get_message())
 
     def datagram_received(self, data, addr):
         LOGGER.debug('data: {}'.format(data))
-
-        try:
-            # parse the KNX header to see what type of KNX message it is
-            header = {}
-            header['header_length'], \
-            header['protocol_version'], \
-            header['service_type'], \
-            header['total_length'] = struct.unpack('>BBHH', data[:6])
-            message_type = int(header['service_type'])
-        except Exception as e:
-            LOGGER.exception(e)
-            self.transport.close()
-            return
+        message_type = get_message_type(data)
 
         if message_type == 0x0206: # CONNECT_RESPONSE
             LOGGER.debug('Parsing KnxConnectResponse')
@@ -79,7 +66,6 @@ class KnxTunnelConnection(asyncio.DatagramProtocol):
                 tunnelling_ack = KnxTunnellingAck(
                     communication_channel=response.body.get('communication_channel_id'),
                     sequence_count=response.body.get('sequence_counter'))
-                tunnelling_ack.pack_knx_message()
                 self.transport.sendto(tunnelling_ack.get_message())
 
 
@@ -109,7 +95,6 @@ class KnxTunnelConnection(asyncio.DatagramProtocol):
         disconnect_request = KnxDisconnectRequest(
             sockname=self.sockname,
             communication_channel=self.communication_channel)
-        disconnect_request.pack_knx_message()
         self.transport.sendto(disconnect_request.get_message())
 
 
@@ -141,101 +126,76 @@ class KnxRoutingConnection(asyncio.DatagramProtocol):
 
 
 class KnxBusMonitor(asyncio.DatagramProtocol):
-
-    # TODO: monitors messages from the bus
-
-    def __init__(self, future, loop=None):
+    """Implementation of bus_monitor_mode and group_monitor_mode."""
+    def __init__(self, future, loop=None, group_monitor=True):
         self.future = future
         self.loop = loop or asyncio.get_event_loop()
         self.transport = None
+        self.group_monitor = group_monitor
         self.tunnel_established = False
         self.communication_channel = None
         self.sequence_count = 0
 
     def connection_made(self, transport):
+        LOGGER.debug('Connection established')
         self.transport = transport
         self.peername = self.transport.get_extra_info('peername')
         self.sockname = self.transport.get_extra_info('sockname')
-
-        LOGGER.debug('Connection established')
-
-        connect_request = KnxConnectRequest(sockname=self.sockname)
-        connect_request.pack_knx_message()
+        if self.group_monitor:
+            # Create a TUNNEL_LINKLAYER layer request (default)
+            connect_request = KnxConnectRequest(sockname=self.sockname)
+        else:
+            # Create a TUNNEL_BUSMONITOR layer request
+            connect_request = KnxConnectRequest(sockname=self.sockname, layer_type=0x80)
         self.transport.sendto(connect_request.get_message())
         # Send CONNECTIONSTATE_REQUEST to keep the connection alive
-        self.loop.call_later(50, self.keep_alive)
+        self.loop.call_later(50, self.knx_keep_alive)
 
     def datagram_received(self, data, addr):
         LOGGER.debug('data: {}'.format(data))
-        try:
-            # parse the KNX header to see what type of KNX message it is
-            header = {}
-            header['header_length'], \
-            header['protocol_version'], \
-            header['service_type'], \
-            header['total_length'] = struct.unpack('>BBHH', data[:6])
-            message_type = int(header['service_type'])
-        except Exception as e:
-            LOGGER.exception(e)
+        knx_message = parse_message(data)
+
+        if not knx_message:
+            LOGGER.error('Invalid KNX message: {}'.format(data))
+            self.knx_tunnel_disconnect()
             self.transport.close()
+            self.future.set_result(None)
             return
 
-        if message_type == 0x0206: # CONNECT_RESPONSE
-            LOGGER.debug('Parsing KnxConnectResponse')
-            response = KnxConnectResponse(data)
-
-            if not response.ERROR:
-                if not self.tunnel_established: # we don't have a tunnel set up yet
+        if isinstance(knx_message, KnxConnectResponse):
+            if not knx_message.ERROR:
+                if not self.tunnel_established:
                     self.tunnel_established = True
-                self.communication_channel = response.body['communication_channel_id']
-            else: # device is not alive and we didn't receive a KnxConnectionResponse, we should abort
-                LOGGER.error('CONNECT_RESPONSE ERROR: {}'.format(response.ERROR))
+                self.communication_channel = knx_message.body.get('communication_channel_id')
+            else:
+                if not self.group_monitor and knx_message.ERROR_CODE == 0x23:
+                    LOGGER.error('Device does not support BUSMONITOR, try --group-monitor instead')
+                else:
+                    LOGGER.error('Connection setup error:', knx_message.ERROR)
                 self.transport.close()
                 self.future.set_result(None)
-
-        elif message_type == 0x0420: # TUNNELLING_REQUEST
-            # KNXnet/IP gateway sends back confirmation messages in TUNNELLING_REQUESTS
-            response = KnxTunnellingRequest(data)
-            self.print_message(response)
-
-            if CEMI_PRIMITIVES[response.body.get('cemi').get('message_code')] == 'L_Data.con':
-                # Only if it's a confirmation message we will respond
+        elif isinstance(knx_message, KnxTunnellingRequest):
+            self.print_message(knx_message)
+            if CEMI_PRIMITIVES[knx_message.body.get('cemi').get('message_code')] == 'L_Data.con' or \
+                    CEMI_PRIMITIVES[knx_message.body.get('cemi').get('message_code')] == 'L_Data.ind':
                 tunnelling_ack = KnxTunnellingAck(
-                    communication_channel=response.body.get('communication_channel_id'),
-                    sequence_count=response.body.get('sequence_counter'))
-                tunnelling_ack.pack_knx_message()
+                    communication_channel=knx_message.body.get('communication_channel_id'),
+                    sequence_count=knx_message.body.get('sequence_counter'))
                 self.transport.sendto(tunnelling_ack.get_message())
-
-
-        elif message_type == 0x0421: # TUNNELLING_ACK
-            LOGGER.debug('Parsing KnxTunnelingAck')
-            response = KnxTunnellingAck(data)
-
-            # TODO: probably not even needed
-            self.sequence_count += 1
-        elif message_type == 0x0208: # CONNECTIONSTATE_RESPONSE
-            LOGGER.debug('Parsing KnxConnectionStateResponse')
-            response = KnxConnectionStateResponse(data)
+        elif isinstance(knx_message, KnxTunnellingAck):
+            self.print_message(knx_message)
+        elif isinstance(knx_message, KnxConnectionStateResponse):
             # After receiving a CONNECTIONSTATE_RESPONSE shedule the next one
-            self.loop.call_later(50, self.keep_alive)
-
-        elif message_type == 0x0209: # DISCONNECT_REQUEST
-            LOGGER.debug('Parsing KnxDisconnectRequest')
-            response = KnxDisconnectResponse(data)
-
+            self.loop.call_later(50, self.knx_keep_alive)
+        elif isinstance(knx_message, KnxDisconnectRequest):
             connect_response = KnxDisconnectResponse(communication_channel=self.communication_channel)
-            connect_response.pack_knx_message()
             self.transport.sendto(connect_response.get_message())
-
-        elif message_type == 0x020a: # DISCONNECT_RESPONSE
-            LOGGER.debug('Parsing KnxDisconnectResponse')
-            response = KnxDisconnectResponse(data)
+        elif isinstance(knx_message, KnxDisconnectResponse):
             self.transport.close()
-        else:
-            LOGGER.error('Unknown message type: '.format(message_type))
-            return
+            self.future.set_result(None)
 
     def print_message(self, message):
+        """A generic message printing function. It defines a format for the monitoring modes."""
         assert isinstance(message, KnxTunnellingRequest)
         format = '[ chan_id: {}, seq_no: {}, message_code: {}, source_addr: {}, dest_addr: {}, tcpi: {}, apci: {} ]'.format(
             message.body.get('communication_channel_id'),
@@ -247,17 +207,16 @@ class KnxBusMonitor(asyncio.DatagramProtocol):
             message.body.get('cemi').get('apci'))
         LOGGER.info(format)
 
-    def keep_alive(self):
+    def knx_keep_alive(self):
+        """Sending CONNECTIONSTATE_REQUESTS periodically to keep the connection alive."""
         connection_state = KnxConnectionStateRequest(
             sockname=self.sockname,
             communication_channel=self.communication_channel)
-        connection_state.pack_knx_message()
         self.transport.sendto(connection_state.get_message())
 
-    def tunnel_disconnect(self):
+    def knx_tunnel_disconnect(self):
         """Close the tunnel connection with a DISCONNECT_REQUEST."""
         disconnect_request = KnxDisconnectRequest(
             sockname=self.sockname,
             communication_channel=self.communication_channel)
-        disconnect_request.pack_knx_message()
         self.transport.sendto(disconnect_request.get_message())
