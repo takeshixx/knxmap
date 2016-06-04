0ae59d7ce519ba8e97a4c23432be43148c2db23b
takeshix <takeshix@adversec.com>
2016-06-04 07:09:02 +0200

    Added simple manufacturer interface

---
+++ libknx/manufacturers.py
@@ -0,0 +1,8 @@
+import json
+
+def get_manufacturer_by_id(id):
+    assert isinstance(id, int)
+    m = json.load(open('libknx/manufacturers.json'))
+    for _m in m.get('manufacturers'):
+        if int(_m.get('knx_manufacturer_id')) == id:
+            return _m.get('name')
