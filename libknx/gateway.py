21af2f558ac90e383a4a2aec8e059191b6972e21
takeshix <takeshix@adversec.com>
2016-07-14 12:07:50 +0200

    KNX constants and documentation added

--- libknx/gateway.py
+++ libknx/gateway.py
@@ -2,6 +2,7 @@
 import asyncio
 import logging
 
+from .core import *
 from .messages import *
 
 __all__ = ['KnxGatewaySearch',
@@ -10,7 +11,9 @@ __all__ = ['KnxGatewaySearch',
 LOGGER = logging.getLogger(__name__)
 
 class KnxGatewaySearch(asyncio.DatagramProtocol):
-
+    """A protocol implementation for searching KNXnet/IP gateways via
+    multicast messages. The protocol will hold a set responses with
+    all the KNXnet/IP gateway responses."""
     def __init__(self, loop=None):
         self.loop = loop or asyncio.get_event_loop()
         self.transport = None
@@ -21,23 +24,24 @@ class KnxGatewaySearch(asyncio.DatagramProtocol):
         self.peername = self.transport.get_extra_info('peername')
         self.sockname = self.transport.get_extra_info('sockname')
         packet = KnxSearchRequest(sockname=self.sockname)
-        self.transport.get_extra_info('socket').sendto(packet.get_message(), ('224.0.23.12', 3671))
+        self.transport.get_extra_info('socket').sendto(packet.get_message(),
+               (KNX_CONSTANTS.get('MULTICAST_ADDR'),
+                KNX_CONSTANTS.get('DEFAULT_PORT')))
 
     def datagram_received(self, data, addr):
         try:
             LOGGER.debug('Parsing KnxSearchResponse')
             response = KnxSearchResponse(data)
-
             if response:
                 self.responses.add((addr, response))
             else:
-                LOGGER.info('Not a valid search response!')
+                LOGGER.debug('Not a valid search response!')
         except Exception as e:
             LOGGER.exception(e)
 
 
 class KnxGatewayDescription(asyncio.DatagramProtocol):
-
+    """Protocol implelemtation for KNXnet/IP description requests."""
     def __init__(self, future, loop=None, timeout=2):
         self.future = future
         self.loop = loop or asyncio.get_event_loop()
@@ -50,7 +54,6 @@ class KnxGatewayDescription(asyncio.DatagramProtocol):
         self.peername = self.transport.get_extra_info('peername')
         self.sockname = self.transport.get_extra_info('sockname')
         self.wait = self.loop.call_later(self.timeout, self.connection_timeout)
-
         packet = KnxDescriptionRequest(sockname=self.sockname)
         self.transport.sendto(packet.get_message())
 
@@ -65,11 +68,10 @@ class KnxGatewayDescription(asyncio.DatagramProtocol):
         try:
             LOGGER.debug('Parsing KnxDescriptionResponse')
             self.response = KnxDescriptionResponse(data)
-
             if self.response:
                 self.future.set_result(self.response)
             else:
-                LOGGER.info('Not a valid description response!')
+                LOGGER.debug('Not a valid description response!')
                 self.future.set_result(False)
         except Exception as e:
             LOGGER.exception(e)
