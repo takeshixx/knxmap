e520452eb1821c4edd4b5d05a1c7eb2b7ae82aaa
takeshix <takeshix@adversec.com>
2016-08-25 23:17:40 +0200

    Added KNX Routing message classes

--- libknxmap/messages.py
+++ libknxmap/messages.py
@@ -1215,6 +1215,80 @@ class KnxDeviceConfigurationAck(KnxMessage):
             LOGGER.exception(e)
 
 
-# TODO: implement routing requests (multicast?)
-#       ROUTING_INDICATION
-#       ROUTING_LOST_MESSAGE
+class KnxRoutingIndication(KnxMessage):
+
+    def __init__(self, message=None):
+        super(KnxRoutingIndication, self).__init__()
+        if message:
+            self.unpack_knx_message(message)
+        else:
+            self.header['service_type'] = KNX_MESSAGE_TYPES.get('ROUTING_INDICATION')
+            self.pack_knx_message()
+
+    def _pack_knx_body(self, cemi=None):
+        if cemi:
+            self.body += cemi
+        else:
+            self.body += self._pack_cemi()
+        return self.body
+
+    def _unpack_knx_body(self, message):
+        try:
+            message = io.BytesIO(message)
+            self.body['cemi'] = self._unpack_cemi(message)
+        except Exception as e:
+            LOGGER.exception(e)
+
+
+class KnxRoutingLostMessage(KnxMessage):
+
+    def __init__(self, message=None):
+        super(KnxRoutingLostMessage, self).__init__()
+        if message:
+            self.unpack_knx_message(message)
+        else:
+            self.header['service_type'] = KNX_MESSAGE_TYPES.get('ROUTING_LOST_MESSAGE')
+            self.pack_knx_message()
+
+    def _pack_knx_body(self):
+        self.body = struct.pack('!B', 4) # structure_length
+        self.body += struct.pack('!B', 0) # device state
+        self.body += struct.pack('!H', 0) # number of lost messages
+        return self.body
+
+    def _unpack_knx_body(self, message):
+        try:
+            message = io.BytesIO(message)
+            self.body['structure_length'] = self._unpack_stream('!B', message)
+            self.body['device_state'] = self._unpack_stream('!B', message)
+            self.body['lost_messages'] = self._unpack_stream('!H', message)
+        except Exception as e:
+            LOGGER.exception(e)
+
+
+class KnxRoutingBusy(KnxMessage):
+
+    def __init__(self, message=None):
+        super(KnxRoutingBusy, self).__init__()
+        if message:
+            self.unpack_knx_message(message)
+        else:
+            self.header['service_type'] = KNX_MESSAGE_TYPES.get('ROUTING_BUSY')
+            self.pack_knx_message()
+
+    def _pack_knx_body(self):
+        self.body = struct.pack('!B', 4) # structure_length
+        self.body += struct.pack('!B', 0) # device state
+        self.body += struct.pack('!H', 0) # routing busy wait time
+        self.body += struct.pack('!H', 0)  # routing busy control field
+        return self.body
+
+    def _unpack_knx_body(self, message):
+        try:
+            message = io.BytesIO(message)
+            self.body['structure_length'] = self._unpack_stream('!B', message)
+            self.body['device_state'] = self._unpack_stream('!B', message)
+            self.body['busy_wait_time'] = self._unpack_stream('!H', message)
+            self.body['busy_control_field'] = self._unpack_stream('!H', message)
+        except Exception as e:
+            LOGGER.exception(e)
