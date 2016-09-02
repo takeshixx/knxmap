faef8b181252ff6541a81d8934ddc98969930e7a
takeshix <takeshix@adversec.com>
2016-09-02 17:27:45 +0200

    Minor argument parsing refactoring

--- knxmap.py
+++ knxmap.py
@@ -24,54 +24,55 @@ LOGGER = logging.getLogger(__name__)
 ARGS = argparse.ArgumentParser(
     description='KNXnet/IP network and bus mapper',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-SUBARGS = ARGS.add_subparsers(dest='cmd', metavar='')
+SUBARGS = ARGS.add_subparsers(dest='cmd')
 
 # General options
-ARGS.add_argument(
-    '-p', '--port', action='store', dest='port', type=int,
-    default=3671, help='UDP port to be scanned')
-ARGS.add_argument(
-    '--auth-key', action='store', dest='auth_key', type=int,
-    default=0xffffffff, help='Authorize key for System 2 and System 7 devices')
-ARGS.add_argument(
-    '--desc-timeout', action='store', dest='desc_timeout', type=int,
-    default=2, help='Timeout in seconds for unicast description responses')
-ARGS.add_argument(
-    '--desc-retries', action='store', dest='desc_retries', type=int,
-    default=3, help='Count of retries for description requests')
-ARGS.add_argument(
-    '-i', '--interface', action='store', dest='iface',
-    default=None, help='Interface to be used')
-ARGS.add_argument(
-    '--workers', action='store', type=int, metavar='N',
-    default=30, help='Limit concurrent workers')
 ARGS.add_argument(
     '-v', '--verbose', action='count', dest='level',
     default=2, help='Verbose logging (repeat for more verbose)')
 ARGS.add_argument(
     '-q', '--quiet', action='store_const', const=0, dest='level',
     default=2, help='Only log errors')
+ARGS.add_argument(
+    '-p', action='store', dest='port', type=int,
+    default=3671, help='UDP port to be scanned')
+ARGS.add_argument(
+    '-i', action='store', dest='iface',
+    default=None, help='Interface to be used')
+ARGS.add_argument(
+    '--workers', action='store', type=int, metavar='N',
+    default=30, help='Limit concurrent workers')
+ARGS.add_argument(
+    '--key', action='store', dest='auth_key', type=int,
+    default=0xffffffff, help='Authorize key for System 2 and System 7 devices')
+ARGS.add_argument(
+    '--timeout', action='store', dest='timeout', type=int,
+    default=2, help='Timeout in seconds for unicast description responses')
+ARGS.add_argument(
+    '--retries', action='store', dest='retries', type=int,
+    default=3, help='Count of retries for description requests')
 
-pscan = SUBARGS.add_parser('scan', help='Scan KNXnet/IP gateways')
+pscan = SUBARGS.add_parser('scan', help='Scan KNXnet/IP gateways and attached bus devices')
 pscan.add_argument(
     'targets', help='KNXnet/IP gateway', metavar='gateway')
 pscan.add_argument(
     '--bus-targets', action='store', dest='bus_targets',
-    default=None, help='Bus target range')
+    default=None, help='Bus target range (e.g. 1.1.0-1.1.10)')
 pscan.add_argument(
     '--bus-info', action='store_true', dest='bus_info',
-    default=False, help='Try to extract information from bus devices')
+    default=False, help='Try to extract information from alive bus devices')
 
-psearch = SUBARGS.add_parser('search', help='Search for KNXnet/IP gateways')
+psearch = SUBARGS.add_parser('search',
+                             help='Search for KNXnet/IP gateways on the local network')
 psearch.add_argument(
     '--search-timeout', action='store', dest='search_timeout', type=int,
     default=5, help='Timeout in seconds for multicast responses')
 
-pwrite = SUBARGS.add_parser('write', help='Write value to group address')
+pwrite = SUBARGS.add_parser('write', help='Write a value to a group address')
 pwrite.add_argument(
     'targets', help='KNXnet/IP gateway', metavar='gateway')
 pwrite.add_argument(
-    'group_write_address', help='A KNX group address')
+    'group_write_address', help='A KNX group address to write to')
 pwrite.add_argument(
     'group_write_value', default=0, help='Value to write to the group address')
 pwrite.add_argument(
@@ -84,12 +85,12 @@ pbrute.add_argument(
 pbrute.add_argument(
     'bus_target', help='Individual address of bus device')
 
-pmonitor = SUBARGS.add_parser('monitor', help='Monitor bus messages')
+pmonitor = SUBARGS.add_parser('monitor', help='Monitor bus and group messages')
 pmonitor.add_argument(
     'targets', help='KNXnet/IP gateway', metavar='gateway')
 pmonitor.add_argument(
     '--group-monitor', action='store_true', dest='group_monitor_mode',
-    default=False, help='Monitor group bus messages via KNXnet/IP gateway')
+    default=False, help='Monitor group instead of messages via KNXnet/IP gateway')
 # TODO: add dump-file argument for monitoring modes
 
 
@@ -122,8 +123,8 @@ def main():
                 target=args.group_write_address,
                 value=args.group_write_value,
                 routing=args.routing,
-                desc_timeout=args.desc_timeout,
-                desc_retries=args.desc_retries,
+                desc_timeout=args.timeout,
+                desc_retries=args.retries,
                 iface=args.iface))
         elif args.cmd == 'monitor':
             loop.run_until_complete(knxmap.monitor(
@@ -135,8 +136,8 @@ def main():
             LOGGER.info('Scanning {} target(s)'.format(len(targets.targets)))
             bus_targets = KnxTargets(args.bus_targets)
             loop.run_until_complete(knxmap.scan(
-                desc_timeout=args.desc_timeout,
-                desc_retries=args.desc_retries,
+                desc_timeout=args.timeout,
+                desc_retries=args.retries,
                 bus_targets=bus_targets.targets,
                 bus_info=args.bus_info,
                 auth_key=args.auth_key))
