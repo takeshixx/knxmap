f39198ce1c2d517e7686d6071a7b7721d1f26376
takeshix <takeshix@adversec.com>
2016-09-04 01:29:33 +0200

    Changed id to prevent shadowing

--- libknxmap/manufacturers.py
+++ libknxmap/manufacturers.py
@@ -1,9 +1,9 @@
 import json
 
 
-def get_manufacturer_by_id(id):
-    assert isinstance(id, int)
+def get_manufacturer_by_id(mid):
+    assert isinstance(mid, int)
     m = json.load(open('libknxmap/data/manufacturers.json'))
     for _m in m.get('manufacturers'):
-        if int(_m.get('knx_manufacturer_id')) == id:
+        if int(_m.get('knx_manufacturer_id')) == mid:
             return _m.get('name')
